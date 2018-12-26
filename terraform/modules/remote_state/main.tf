cd8ef96b128e66c7a237428b4e8d4c9b365a44e9
Richard Slater <webmaster@slaterfamily.name>
2018-12-26 16:18:33 +0000

    Create remote state bucket with lifecycle.

--- terraform/modules/remote_state/main.tf
+++ terraform/modules/remote_state/main.tf
@@ -1,8 +1,21 @@
 # modules/remote_state/main.tf
 resource "google_storage_bucket" "remote-state" {
   name     = "${var.admin_project}"
-  location = "europe-west2"
+  location = "eu"
+  storage_class = "MULTI_REGIONAL"
   force_destroy = true
+  versioning {
+    enabled = true
+  }
+  lifecycle_rule {
+    condition {
+      age = 28
+      num_newer_versions = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
 }
 
 resource "google_storage_bucket_acl" "remote-state-acl" {
